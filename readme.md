51861d9695df8d84ce51fc398e4e89d183308257
polpa-web <marta.toso01@universitadipavia.it>
2025-05-28 16:56:16 +0200

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,4 @@
+# POLPAWEB.COM 🐙🦑  
+The squiddiest website on the internet! Click to reveal an amazingly wonderful image of an octopus or squid!  
+If they are in love...💖 Magic will appear!
+EVVIVA I POLPINI! DANKJEWEL VOOR DE FIETS!
---
+++ script.js
@@ -0,0 +1,17 @@
+async function loadImage() {
+  const response = await fetch("https://api.unsplash.com/photos/random?query=octopus,squid&client_id=Gtm9c4Em02v8-yvl__DK70bRYOwOnoOmJGbvTFF0TUY");
+  const data = await response.json();
+
+  const imgElement = document.getElementById("octopus-image");
+  imgElement.src = data.urls.regular;
+
+  const isLove = data.alt_description?.toLowerCase().includes("love") || data.description?.toLowerCase().includes("love");
+
+  const hearts = document.getElementById("hearts");
+  if (isLove) {
+    hearts.style.display = "block";
+    setTimeout(() => hearts.style.display = "none", 1500);
+  } else {
+    hearts.style.display = "none";
+  }
+}
